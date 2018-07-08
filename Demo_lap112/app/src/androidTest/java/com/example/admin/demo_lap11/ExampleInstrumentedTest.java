9325c1f125deac6694aa86f14a59ff5baf8a3fcf
windboy9x <windboy9x@gmail.com>
2018-07-08 23:07:17 +0700

    dfgdfgdfdffgdfgdfg

--- Demo_lap112/app/src/androidTest/java/com/example/admin/demo_lap11/ExampleInstrumentedTest.java
+++ Demo_lap112/app/src/androidTest/java/com/example/admin/demo_lap11/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.admin.demo_lap11;
+package com.example.admin.demo_lap11;?trtertertretertert
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
